dc8ebfdb06687b1955b77f4c273cfc01cdb9f5e7
Aaron Robinson <aaronrobin1234@gmail.com>
2021-06-06 18:31:41 -0500

    Initial commit

---
+++ HacknetPluginTemplate.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Hacknet;
+
+namespace HacknetPluginTemplate
+{
+    [BepInPlugin(ModGUID, ModName, ModVer)]
+    public class HacknetPluginTemplate : HacknetPlugin
+    {
+        public const string ModGUID = "com.Windows10CE.Template";
+        public const string ModName = "If you leave these the same, I will kill you.";
+        public const string ModVer = "1.0.0";
+
+        public override bool Load()
+        {
+            return true;
+        }
+    }
+}

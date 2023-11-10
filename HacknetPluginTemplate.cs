ce58dfb7569e3694e7cd1a92c9635d4f1f2da341
Aaron Robinson <aaronrobin1234@gmail.com>
2023-11-10 15:08:41 -0600

    Change to file-scoped

--- HacknetPluginTemplate.cs
+++ HacknetPluginTemplate.cs
@@ -1,18 +1,17 @@
 ﻿using BepInEx;
 using BepInEx.Hacknet;
 
-namespace HacknetPluginTemplate
-{
-    [BepInPlugin(ModGUID, ModName, ModVer)]
-    public class HacknetPluginTemplate : HacknetPlugin
-    {
-        public const string ModGUID = "com.Windows10CE.Template";
-        public const string ModName = "If you leave these the same, I will kill you.";
-        public const string ModVer = "1.0.0";
+namespace HacknetPluginTemplate;
 
-        public override bool Load()
-        {
-            return true;
-        }
+[BepInPlugin(ModGUID, ModName, ModVer)]
+public class HacknetPluginTemplate : HacknetPlugin
+{
+    public const string ModGUID = "com.Windows10CE.Template";
+    public const string ModName = "If you leave these the same, I will kill you.";
+    public const string ModVer = "1.0.0";
+
+    public override bool Load()
+    {
+        return true;
     }
 }
